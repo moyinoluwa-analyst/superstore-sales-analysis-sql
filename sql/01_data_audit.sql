42a6285962dacf55046940a33d6c43c8462c23ab
moyinoluwa-analyst <odeyemimoyinoluwa87@gmail.com>
2026-06-24 22:57:52 +0100

    Update 01_data_audit.sql

--- sql/01_data_audit.sql
+++ sql/01_data_audit.sql
@@ -1,12 +1,31 @@
+-- Total Rows
+
 SELECT COUNT(*) AS total_rows
 FROM orders;
+
+-- Date Range
 
 SELECT
 MIN(`Order Date`) AS first_order,
 MAX(`Order Date`) AS last_order
 FROM orders;
 
+-- Revenue Overview
+
 SELECT
 ROUND(SUM(Sales),2) AS total_sales,
-ROUND(SUM(Profit),2) AS total_profit
+ROUND(SUM(Profit),2) AS total_profit,
+SUM(Quantity) AS total_quantity
+FROM orders;
+
+-- Total Customers
+
+SELECT
+COUNT(DISTINCT `Customer ID`) AS total_customers
+FROM orders;
+
+-- Total Products
+
+SELECT
+COUNT(DISTINCT `Product ID`) AS total_products
 FROM orders;
